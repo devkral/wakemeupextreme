1774cd27ee60ca4726cde9999604ee147643ee18
alex <devkral@web.de>
2017-01-22 18:22:36 +0100

    fix license and other stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python3
-"""
-license: PD
-"""
+__license__ = "Public Domain"
 
 from setuptools import setup
 
@@ -11,12 +9,6 @@ version = "0.1"
 entry_points = {"gui_scripts": []}
 
 install_requirements = []
-# for certificate generation
-install_requirements += ["cryptography>=1.1"]
-# for pidlock
-install_requirements += ["psutil>=3.0"]
-# require speed by default
-install_requirements += speed_requirements
 
 install_extras = {}
 # for markdown help
@@ -29,21 +21,19 @@ setup(name='wakemeupextreme',
       version=version,
       #version_format='{tag}',
       description='Wake me up with math',
-      license='Public Domain',
+      license='MIT',
+      author='Alexander K.',
+      author_email='devkral@web.de',
       url='https://github.com/devkral/wakemeupextreme',
       download_url='https://github.com/devkral/wakemeupextreme/tarball/'+version,
       entry_points=entry_points,
       #zip_safe=True,
       platforms='Platform Independent',
-      include_package_data=True,
       install_requires=install_requirements,
       extras_require=install_extras,
       packages=['wakemeupextreme'],
       classifiers=[
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3 :: Only',
-          'Topic :: Communications',
-          'Topic :: Internet',
-          'Topic :: Security'],
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3 :: Only'],
       keywords=['alarm', 'clock'])
